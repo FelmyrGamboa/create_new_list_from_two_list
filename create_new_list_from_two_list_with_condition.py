07bb49c4fdcdfb96d9184defec17090fe62b90be
FelmyrGamboa <felmyrgamboa23@gmail.com>
2024-01-24 01:17:20 +0800

    Some revisions to make the code work and display the result

--- create_new_list_from_two_list_with_condition.py
+++ create_new_list_from_two_list_with_condition.py
@@ -16,16 +16,16 @@
 #Create a function that would check the list of odd numbers
 def check_numbers_if_odd(number_list):
 #Create a for loop with a condition if the number is odd
-    for numbers in number_list():
+    for numbers in number_list:
         if numbers % 2 != 0:
-            result_list.append()
+            result_list.append(numbers)
 
 #Create a function that would check the list of even numbers
 def check_numbers_if_even(number_list):
 #Create a for loop with a condition if the number is even
-    for numbers in number_list():
+    for numbers in number_list:
         if numbers % 2 == 0:
-            result_list.append()
+            result_list.append(numbers)
 
 #Create a list that would store the results of the functions
 result_list = list()
@@ -39,3 +39,4 @@ check_numbers_if_odd(number_list_1)
 check_numbers_if_even(number_list_2)
 
 #Display the result
+print("Result list: ", result_list)
